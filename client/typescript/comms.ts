f5627bedf9da5c36e2427dbc51bd781651530424
Yessica-C <41839196+Yessica-C@users.noreply.github.com>
2024-05-04 20:00:50 -0500

    Add files via upload

---
+++ client/typescript/comms.ts
@@ -0,0 +1,26 @@
+async function getMap(mapname: string) {
+    console.log("getMap() invoked");
+
+    // Hardcoded building name for now
+    mapname = "Town Hall";
+
+    // Make HTTP request with mapname labeled as "map" in JSON
+    let labels = {map: mapname};
+    let url = "http://localhost:8080/maprequest"
+    // let request = new Request(url, );
+
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(labels),
+    })
+    const data = await response.json();
+    console.log(data);
+
+    console.log("Request sent successfully");
+}
